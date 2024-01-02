58ca5149f91bd3ed1df7e84f8be150e760bccf57
javibeat <javibeat@gmail.com>
2024-01-02 17:32:21 +0400

    js

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 document.addEventListener("DOMContentLoaded", function() {
-        // Cargar el menú desde menu.html
-        var xhr = new XMLHttpRequest();
-        xhr.onload = function() {
-            if (this.status == 200) {
-                document.getElementById("navbar-container").innerHTML = this.responseText;
+    // Cargar el menú desde menu.html
+    var xhr = new XMLHttpRequest();
+    xhr.onload = function() {
+        if (this.status == 200) {
+            document.getElementById("navbar-container").innerHTML = this.responseText;
 
-                // Una vez cargado el menú, añadir interactividad al menú hamburguesa
-                const hamburger = document.querySelector('.hamburger');
-                const menu = document.querySelector('.menu');
+            // Interactividad del menú móvil
+            const hamburger = document.querySelector('.hamburger');
+            const menu = document.querySelector('.menu');
 
+            if (hamburger) {
                 hamburger.addEventListener('click', function() {
                     menu.classList.toggle('nav-active');
-
-                    // Transformar el icono de hamburguesa en una X
                     this.classList.toggle('toggle');
                 });
             }
-        };
-        xhr.open("GET", "menu.html", true);
-        xhr.send();
-    });
+        }
+    };
+    xhr.open("GET", "menu.html", true);
+    xhr.send();
+});
